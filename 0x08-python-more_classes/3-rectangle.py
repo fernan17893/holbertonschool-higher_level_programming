88b127df45b023c8314f8a818d37ec6c3af1c869
hoodrich-fern <fernan17893@gmail.com>
2022-05-24 01:06:29 +0000

    task 3 fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
     def __str__(self):
         new_str = ("")
         if self.__width == 0 or self.__height == 0:
-            return (" ")
+            return (new_str)
         else:
             for i in range(self.__height):
                 for j in range(self.__width):
