fa2bc2ddede082883b7922029f066420e256afb6
hoodrich-fern <fernan17893@gmail.com>
2022-06-01 19:59:46 +0000

    big push

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Defines class MyInt which inherits from int """
+
+
+class MyInt(int):
+    """ class that inherits from int"""
+
+    def __eq__(self, other):
+        return False
+
+
+    def __ne__(self,other):
+        return True
