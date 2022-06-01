80574ca3b20572bf3b359fe1738b0358d032bf28
hoodrich-fern <fernan17893@gmail.com>
2022-06-01 15:54:44 +0000

    task 8 cause i ruined it

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,21 +12,19 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ Public instance method validates if value is an integer and greater
-    than 0, if not TypeError is raised for non integer value, and
-    ValueError raised if value is less than or equal to 0
-    """
-
-    if not isinstance(value, int):
-        raise TypeError("<name> must be an integer")
-    elif value <= 0:
-        raise ValueError("<name> must be greater than 0")
-
+        than 0, if not TypeError is raised for non integer value, and
+        ValueError raised if value is less than or equal to 0
+     """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """ Rectangle subclass inherited from BaseGeometry class """
 
     def __init__(self, width, height):
-        self.integer_validator(self, width)
+        self.integer_validator("width", width)
         self.width = width
-        self.integer_validator(self, height)
+        self.integer_validator("height", height)
         self.height = height
