9dfecd68fb73993a7ef17f085c5af97e760311d4
hoodrich-fern <fernan17893@gmail.com>
2022-06-08 14:20:48 +0000

    dic representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,3 +102,6 @@ class Rectangle(Base):
         elif len(kwargs) > 0:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {'id': self.id , 'width': self.__width , 'height': self.__height , 'x': self.__x , 'y': self.__y} 
