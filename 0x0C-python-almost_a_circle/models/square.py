d061adaa71e7ed33fa3f65a9357e8e92f8daa754
hoodrich-fern <fernan17893@gmail.com>
2022-06-08 02:30:33 +0000

    square output

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,9 +2,6 @@
 Class Square that inherits from Rectangle
 Base super class
 """
-
-
-from models.base import Base
 from models.rectangle import Rectangle
 
 
@@ -12,6 +9,9 @@ class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = self.width
+
+
 
     def __str__(self):
         return ('[Square] (' + str(self.id) + ') ' + str(self.x) + '/' +
