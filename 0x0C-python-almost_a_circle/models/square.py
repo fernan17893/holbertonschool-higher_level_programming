34f9919663a2456fe4f710933b44c7b8e5631fa0
hoodrich-fern <fernan17893@gmail.com>
2022-06-08 03:17:10 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,25 @@ class Square(Rectangle):
         """ __str__ method update """
         return ('[Square] (' + str(self.id) + ') ' + str(self.x) + '/' +
                 str(self.y) + ' - ' + str(self.width))
+
+    @property
+    def size(self):
+        """property to retrieve private instance size"""
+        return (self.width)
+
+    @size.setter
+    def size(self, size):
+        """to set private instance size"""
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """ public methods assings attributes """
+        if len(args) > 0:
+            my_list = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if i < len(my_list):
+                    setattr(self, my_list[i], args[i])
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
