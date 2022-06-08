31cfef1e89e03a88cb377b0ffa7c9231de9515d1
hoodrich-fern <fernan17893@gmail.com>
2022-06-08 02:32:44 +0000

    please work

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,12 +7,13 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Class Square"""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """ Constructor for square """
         super().__init__(size, size, x, y, id)
         self.size = self.width
 
-
-
     def __str__(self):
+        """ __str__ method update """
         return ('[Square] (' + str(self.id) + ') ' + str(self.x) + '/' +
                 str(self.y) + ' - ' + str(self.width))
