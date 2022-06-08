78d9f39e457af10791641302663bc3ece05a3f42
hoodrich-fern <fernan17893@gmail.com>
2022-06-08 01:45:51 +0000

    rectangle fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+""" Class Rectangle that inherits from Base """
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ Class Rectangle """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
+        """ property retireves width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ sets private instance width """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,11 +28,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ property retrieves instace height """
         return self.__height
 
 
     @height.setter
     def height(self, value):
+        """ set private instance height """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -37,10 +43,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ retrieve private instance x """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ to set private instance x"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -49,10 +57,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ retrieve private instance y """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ set private instance y """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -61,22 +71,18 @@ class Rectangle(Base):
 
 
     def area(self):
+        """ return rectangle area """
         return self.height * self.width
 
     def display(self):
+        """ print # symbol """
         for i in range(self.height):
             [print("#", end ="") for j in range(self.width)]
             print("")
 
     def __str__(self):
-        return (f"[Rectangle] (<id>) <x>#!/usr/bin/python3
-""" 5-main """
-from models.rectangle import Rectangle
+        """ updates class overiding __str__ method """
+        str = (f"({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+        return (f"[Rectangle] {str}")
 
-if __name__ == "__main__":
 
-    r1 = Rectangle(4, 6, 2, 1, 12)
-    print(r1)
-
-    r2 = Rectangle(5, 5, 1)
-    print(r2)/<y> - <width>/<height>")
