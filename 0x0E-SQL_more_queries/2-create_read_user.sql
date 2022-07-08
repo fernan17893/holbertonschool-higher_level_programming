748f913283a57059da7c98c65888c9a374fff18c
fernan17893 <fernan17893@gmail.com>
2022-07-08 01:21:38 +0000

    test commit task 0-3

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- creates database hbtn_0d_2 and user_0d_2
+-- user_0d_2 should have SELECT privilege in database
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+    ON `hbtn_0d_2`.*
+    TO 'user_0d_2'@'localhost'
