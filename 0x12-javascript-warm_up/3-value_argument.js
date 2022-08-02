de3f1833dacd676bd61785528dda73048c903bc9
fernan17893 <fernan17893@gmail.com>
2022-08-02 13:33:55 +0000

    semi fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const myVar = process.argv.length;
 if (myVar === 3) { console.log(process.argv[2]); }
-if (myVar < 3) { console.log("No argument"); }
+if (myVar < 3) { console.log('No argument'); }
