3f7220daccdb1593aa1d18a76001943c72ebe5e4
fernan17893 <fernan17893@gmail.com>
2022-08-03 03:40:13 +0000

    4 finished

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,7 +3,19 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
+
   print () {
-	for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
-};  
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
