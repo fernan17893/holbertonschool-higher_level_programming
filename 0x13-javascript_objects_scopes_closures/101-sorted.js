2f9361b64e10981217113df70e4464496ad9769f
fernan17893 <fernan17893@gmail.com>
2022-08-04 02:26:08 +0000

    adv task

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dic = require('./101-data.js').dict;
+const newDic = {};
+for (const key in dic) {
+  if (newDic[dic[key]] === undefined) {
+    newDic[dic[key]] = [key];
+  } else {
+    newDic[dic[key]].push(key);
+  }
+}
+console.log(newDic);
