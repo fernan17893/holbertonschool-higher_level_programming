715248bf770fc00d2923c3c52bf787df62b7d036
fernan17893 <fernan17893@gmail.com>
2022-08-04 03:09:43 +0000

    adv task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-console.log(list)
-console.log(list.map((element, index) => element * index))
+console.log(list);
+console.log(list.map((element, index) => element * index));
