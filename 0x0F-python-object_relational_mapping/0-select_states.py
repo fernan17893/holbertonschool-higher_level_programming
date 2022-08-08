515bbcc2460c13d925b250f0c04e7319a0dceec5
fernan17893 <fernan17893@gmail.com>
2022-08-08 02:52:54 +0000

    pycode

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,12 +4,13 @@ list all states from database
 """
 from sys import argv
 import MySQLdb
-db = MySQLdb.connect(host="localhost", port=3306, user="root",
-                     passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user="root",
+                         passwd=argv[2], db=argv[3])
 
-cur = db.cursor()
-cur.execute("select * FROM states order by id ASC")
-for row in cur.fetchall():
-    print(row)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("select * FROM states order by id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
