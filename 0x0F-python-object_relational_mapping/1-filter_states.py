710dcbbbf20bc2b63653c55c63010037cb24404a
fernan17893 <fernan17893@gmail.com>
2022-08-08 03:34:19 +0000

    pycode

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute("select * FROM states\
-    WHERE name like 'N%'
+    WHERE name like 'N%'\
     ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
