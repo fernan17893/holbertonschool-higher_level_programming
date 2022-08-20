a9b9111b754d45bcf6c7a4b70756ae9e64370dd4
fernan17893 <fernan17893@gmail.com>
2022-08-20 11:13:40 -0400

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Display value of header X-Request-Id """
 import sys
-from urllib.request import Request, urlopen
+import urllib.request 
 
-if __name__ == "__main__":
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        pass
-print(response.getheader('X-Request-Id'))
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        print(response.getheader('X-Request-Id'))
