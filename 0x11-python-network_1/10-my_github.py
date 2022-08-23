78743f9c683b1156858f953e227bcf8c58316488
fernan17893 <fernan17893@gmail.com>
2022-08-23 12:01:47 -0400

    pls work

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,8 +6,9 @@ import requests
 
 
 if __name__ == "__main__":
+    user = sys.argv[1]
+    passw = sys.argv[2]
 
-    r = requests.get('https://api.github.com/user', auth=(sys.argv[1],sys.argv[2]))
-    rson = r.json()
-
-    print(rson.get("id"))
+r = requests.get('https://api.github.com/user', auth=(user, passw))
+rson = r.json()
+print(rson.get("id"))
