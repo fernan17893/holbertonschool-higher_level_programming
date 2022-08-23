ab8b269e999ae00831e34b89c63d0ffa88d92499
fernan17893 <fernan17893@gmail.com>
2022-08-23 13:18:07 -0400

    found the typo

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Script takes Python credentials and displays id
+""" Script takes Python credentials and displays id """
 import sys
 import requests
 
@@ -9,3 +9,5 @@ if __name__ == "__main__":
     passw = sys.argv[2]
 r = requests.get('https://api.github.com/user', auth=(user, passw))
 print(r.json().get("id"))
+
+HEY WHATS UP THIS IS CHRIS
