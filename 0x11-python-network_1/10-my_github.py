4b70900e3da9db696d8785ef35940883145cd3a5
fernan17893 <fernan17893@gmail.com>
2022-08-23 13:30:22 -0400

    l

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     base = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-r = requests.get("https://api.github.com/user", auth=base)
-print(r.json().get("id"))
+    r = requests.get("https://api.github.com/user", auth=base)
+    print(r.json().get("id"))
