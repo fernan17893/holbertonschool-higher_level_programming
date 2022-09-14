dbb070f2c6c29f01274be75091429f4dddd48298
fernan17893 <fernan17893@gmail.com>
2022-09-14 15:12:26 -0400

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,10 @@
 
 const axios = require('axios');
 
-axios.get(process.argv[2]).then(resp => {
-  console.log('code: ', resp.status);
-});
+axios.get(process.argv[2])
+  .then(resp => {
+    console.log('code: ', resp.status);
+  })
+  .catch(error => {
+    console.log('code: ', error.response.status);
+  });
