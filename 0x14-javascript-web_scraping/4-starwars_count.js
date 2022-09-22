c309b6fe35139a97118f0816d897aca6fecc35b0
fernan17893 <fernan17893@gmail.com>
2022-09-22 10:47:45 -0400

    forgoten tasks commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*Script prints the number of movies where Wedge Antilles is present */
+
+const axios = require('axios');
+let appearance = 0;
+axios.get(process.argv[2])
+  .then(res => {
+    for (let blockIndex = 0; blockIndex < res.data.results.length; blockIndex++) {
+      if (res.data.results[blockIndex]) {
+        for (let characterIndex = 0; characterIndex < res.data.results[blockIndex].characters.length; characterIndex++) {
+          if (res.data.results[blockIndex].characters[characterIndex].includes('18')) {
+            appearance++;
+          }
+        }
+      }
+    }
+    console.log(appearance);
+  })
+  .catch(error => {
+    if (error.response) {
+      console.log(`code: ${error.response.status}`);
+    }
+  });
+
